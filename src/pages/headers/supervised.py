9d0e79ba6f8257832bbff3b6eb2a8f0202040486
Kyle McLester <kmcleste@uncc.edu>
2022-09-11 13:49:27 -0400

    Updated filenames, created util functions, formalize page layouts.. sorta

--- src/pages/headers/supervised.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def supervised_header():
-    with st.expander(label="Supervised Learning", expanded=False):
-        st.write("Expanded")
--- src/pages/headers/unsupervised.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def unsupervised_header():
-    with st.expander(label="Unsupervised Learning", expanded=False):
-        st.write("Expanded")
