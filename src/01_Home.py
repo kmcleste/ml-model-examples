f7b71a6cc049612f37a177b12476252321060b50
Kyle McLester <kmcleste@uncc.edu>
2022-09-11 11:35:47 -0400

    Setup basic page structure

---
+++ src/01_Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home", page_icon="🏠", layout="centered", initial_sidebar_state="auto"
+)
+
+st.markdown("# Home")
---
+++ src/pages/tabs/tab.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def tab1():
+    st.write("tab 1")
+
+
+def tab2():
+    st.write("tab 2")
